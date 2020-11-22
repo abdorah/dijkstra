4958670a4787abe5b13a5117dc118cc87bcb05ac
Kotbi Abderrahmane <kotbymo@gmail.com>
2020-11-23 00:31:43 +0100

    finish the project. Not tested yet.

--- src/main/java/dijkstra/Graph.java
+++ src/main/java/dijkstra/Graph.java
@@ -1,5 +1,7 @@
 package dijkstra;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -12,19 +14,40 @@ public class Graph {
     Set<String> vertexes;
     List<Set<String>> listOfLinks;
 
-    public Edge closerVertex(String vertex){
+    public String closestVertex(String vertex) {
+        float distance = Float.MAX_VALUE;
+        String minDistantVertex = null;
+        // Set<String> link;
+        // for(Set<String> l : this.graph.listOfLinks){
+        // link.add(vertex);
+        // link.add(this.);
+        // link = null;
+        // }
+        // this.graph.stream().filter(v->v.equals(vertex)).collect(collector)
+        for (Edge e : this.graph) {
+            if (e.vertexIsInEdge(vertex)) {
+                if (distance >= e.distance) {
+                    distance = e.distance;
+                    minDistantVertex = vertex == e.vertex1 ? e.vertex2 : e.vertex1;
+                }
+            }
+        }
+        return Optional.ofNullable(minDistantVertex).orElseThrow(GraphNotConnectedException::new);
+    }
+
+    public Edge closestEdge(String vertex) {
         float distance = Float.MAX_VALUE;
         Edge minDistantEdge = null;
         // Set<String> link;
         // for(Set<String> l : this.graph.listOfLinks){
-        //     link.add(vertex);
-        //     link.add(this.);
-        //     link = null;
+        // link.add(vertex);
+        // link.add(this.);
+        // link = null;
         // }
         // this.graph.stream().filter(v->v.equals(vertex)).collect(collector)
-        for(Edge e : this.graph){
-            if(e.vertexIsInEdge(vertex)){
-                if(distance >= e.distance){
+        for (Edge e : this.graph) {
+            if (e.vertexIsInEdge(vertex)) {
+                if (distance >= e.distance) {
                     distance = e.distance;
                     minDistantEdge = e;
                 }
@@ -33,8 +56,6 @@ public class Graph {
         return Optional.ofNullable(minDistantEdge).orElseThrow(GraphNotConnectedException::new);
     }
 
-    String previous;
-
     public Graph(Set<Edge> graph, String source) {
         this.graph = graph;
         this.source = source;
@@ -42,11 +63,31 @@ public class Graph {
         this.vertexes.addAll(graph.stream().map(Edge::getVertex2).collect(Collectors.toSet()));
         this.listOfLinks.addAll(graph.stream().map(Edge::getLink).collect(Collectors.toList()));
     }
-    
-    public void dijkstra(Graph graph, String source){
-        Set<String> buffer;
-        vertexes.stream().forEach(v->{
 
-        });
+    public HashMap<Float,List<String>> dijkstra(String source) {
+        Set<String> bufferes = this.vertexes;
+        String buffer = source;
+        // String bufferToRemove = null;
+        List<String> previous = new ArrayList<>();
+        float distance = 0;
+        HashMap<Float,List<String>> result = null;
+        // float distance = Float.MAX_VALUE;
+        // float sourceDistance = 0;
+        // this.graph.stream().forEach(e->{
+
+        // });
+        while(!(bufferes.isEmpty())){
+            buffer = this.closestVertex(buffer);
+            bufferes.remove(buffer);
+            for(Edge e : this.graph){
+                if(e.vertexIsInEdge(buffer)){
+                    //bufferToRemove = buffer == e.vertex1 ? e.vertex2:e.vertex1;
+                    distance = distance + e.distance;
+                    previous.add(buffer);
+                } 
+            }
+        }
+        result.put(distance, previous);
+        return Optional.ofNullable(result).orElseThrow(GraphNotConnectedException::new);
     }
 }    
